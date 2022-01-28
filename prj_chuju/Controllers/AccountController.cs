7de9da3040d713c3e8843e9d5e6ec316b09e5910
Budfish <87319891+Budfish@users.noreply.github.com>
2022-01-28 12:39:20 +0800

    會員登入頁面
    將會員登入頁面加入至專案中。

---
+++ prj_chuju/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prj_chuju.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult loginPage()
+        {
+            return View();
+        }
+    }
+}

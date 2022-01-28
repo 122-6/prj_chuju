6f48c57bdb2c05d7a4ba98051a3ed72015c6ace7
Budfish <87319891+Budfish@users.noreply.github.com>
2022-01-28 14:42:35 +0800

    更新範本
    1.以買房第一站為範本，我做了一個簡易的ADO連線方法。
    負責後端資料庫連結的成員可以參考。
    
    2. 如果資料庫裡存的URL含有中文，在用razor語法取出路徑的時候會有問題。故將原本的「總覽文章圖片」資料夾改名為「articleListPictures」並修正資料庫的資料。

--- prj_chuju/Controllers/BrandArticleController.cs
+++ prj_chuju/Controllers/BrandArticleController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using prj_chuju.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,10 +10,35 @@ namespace prj_chuju.Controllers
 {
     public class BrandArticleController : Controller
     {
+        static SqlConnectionStringBuilder myBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = "chujudbserver.database.windows.net",
+            InitialCatalog = "dbchuju",
+            UserID = "chujuas",
+            Password = "P@ssw0rd-chuju",
+            AsynchronousProcessing = true,
+        };
+        static string conStr = myBuilder.ToString();
+
         // GET: BrandArticle
         public ActionResult Index()
         {
-            IEnumerable<articleOutline> list = from p in (new dbtest01Entities()).articleOutline select p;
+            List<class_articleOutline> list = new List<class_articleOutline>();
+
+            SqlConnection con = new SqlConnection(conStr);
+            SqlCommand cmd;
+            SqlDataReader reader;
+
+            con.Open();
+            cmd = new SqlCommand("select * from articleOutline",con);
+            reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                class_articleOutline ao = new class_articleOutline(reader);
+                list.Add(ao);
+            }
+            con.Close();
+
             return View(list);
         }
     }
---
+++ prj_chuju/Models/class_articleOutline.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace prj_chuju.Models
+{
+    public class class_articleOutline
+    {
+        public int id;
+        public int articleClass;
+        public DateTime publishDate;
+        public string title;
+        public string intro;
+        public string articleSource;
+        public string pictureURL;
+
+        public class_articleOutline() { }
+        public class_articleOutline(SqlDataReader reader)
+        {
+            id = Convert.ToInt32(reader["id"]);
+            articleClass = Convert.ToInt32(reader["articleClass"]);
+            publishDate = Convert.ToDateTime(reader["publishDate"]);
+            title = reader["title"].ToString();
+            intro = reader["intro"].ToString();
+            articleSource = reader["articleSource"].ToString();
+            pictureURL = reader["pictureURL"].ToString();
+        }
+    }
+}

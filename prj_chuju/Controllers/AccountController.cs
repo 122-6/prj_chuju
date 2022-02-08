6e68f14d26a2914c267d8fb684c12b67a869dd43
Budfish <87319891+Budfish@users.noreply.github.com>
2022-02-08 16:13:10 +0800

    註冊會員功能更新
    新增手機驗證、信箱驗證、以及寄送驗證信功能

--- prj_chuju/Controllers/AccountController.cs
+++ prj_chuju/Controllers/AccountController.cs
@@ -4,6 +4,13 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+// email相關
+using System.Net;
+using System.Net.Mail;
+
+// sql相關
+using System.Data.SqlClient;
+
 namespace prj_chuju.Controllers
 {
     public class AccountController : Controller
@@ -17,6 +24,42 @@ namespace prj_chuju.Controllers
         public ActionResult loginPage()
         {
             return View();
+        }
+
+        [HttpPost]
+        public void sendMail()
+        {
+            string theMail = Request["theMail"];
+            string theSecret = Request["theSecret"];
+
+            using (SmtpClient email = new SmtpClient
+            {
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                EnableSsl = true,
+                Host = "smtp.gmail.com",
+                Port = 587,
+                Credentials = new NetworkCredential("chujumail306@gmail.com", "P@ssw0rd-chuju"),
+            })
+            {
+                string subject = $"雛居帳號驗證碼";
+                string body = $"感謝您註冊雛居廣告帳戶！\n\n您的驗證碼為: {theSecret}\n\n於 {DateTime.Now} 發送";
+                try
+                {
+                    email.Send("chujumail306@gmail.com", theMail, subject, body);
+                }
+                catch (SmtpException ex)
+                {
+                    string msg = ex.StackTrace.ToString();
+                }
+            }
+        }
+
+        [HttpPost]
+        public ActionResult create()
+        {
+            
+            return RedirectToAction("Index", "Home");
         }
     }
 }
--- prj_chuju/ViewModels/ArticleContentViewModel.cs
+++ prj_chuju/ViewModels/ArticleContentViewModel.cs
@@ -7,8 +7,10 @@ namespace prj_chuju.ViewModels
 {
     public class ArticleContentViewModel
     {
+        
         public List<articleOutline> articleOutline { get; set; }
         public List<articleContent> articleContent { get; set; }
         public List<articleOutline> articleDetail { get; set; }
+        
     }
 }

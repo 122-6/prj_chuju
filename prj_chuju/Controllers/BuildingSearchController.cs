13f101595352813b8bfe5144504fb987ab8ee3ff
dennylu <denny120120@gmail.com>
2022-02-10 18:48:20 +0800

    建案搜尋列表

---
+++ prj_chuju/Controllers/BuildingSearchController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prj_chuju.Controllers
+{
+    public class BuildingSearchController : Controller
+    {
+        // GET: BuildingSearch
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

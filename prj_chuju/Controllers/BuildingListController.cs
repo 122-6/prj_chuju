7376e8d4d8349e732a151c46a09e551e5eef0894
dennylu <denny120120@gmail.com>
2022-02-19 19:47:58 +0800

    建案列表連動資料庫

--- prj_chuju/Controllers/BuildingListController.cs
+++ prj_chuju/Controllers/BuildingListController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using prj_chuju.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,10 +10,39 @@ namespace prj_chuju.Controllers
 {
     public class BuildingListController : Controller
     {
+        static SqlConnectionStringBuilder myBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = "chujudbserver.database.windows.net",
+            InitialCatalog = "dbchuju",
+            UserID = "chujuas",
+            Password = "P@ssw0rd-chuju",
+            AsynchronousProcessing = true,
+        };
+        static string conStr = myBuilder.ToString();
+
+        
         // GET: BuildingList
+        
+
         public ActionResult Index()
         {
-            return View();
+            List<class_buildingList> list = new List<class_buildingList>();
+
+            SqlConnection con = new SqlConnection(conStr);
+            SqlCommand cmd;
+            SqlDataReader reader;
+
+            con.Open();
+            cmd = new SqlCommand("select * from buildingdb", con);
+            reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                class_buildingList bl  = new class_buildingList(reader);
+                list.Add(bl);
+            }
+            con.Close();
+
+            return View(list);
         }
     }
 }

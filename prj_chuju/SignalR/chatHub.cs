d6200f46c5b87c1de1761194027e2342110ddcee
Gary1235 <gary19961221@gmail.com>
2022-02-24 10:06:09 +0800

    新增客服系統

---
+++ prj_chuju/SignalR/chatHub.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace prj_chuju.SignalR
+{
+    public class chatHub : Hub
+    {
+        private static string serviceID;
+        private static string clientID;
+        private static int count = 0;
+        public void ServiceConnection()
+        {
+            serviceID = Context.ConnectionId;
+        }
+
+        public void ClientConnection()
+        {
+            clientID = Context.ConnectionId;
+            Clients.Client(serviceID).newClient(clientID, count);
+            Clients.Client(clientID).newClient(clientID);
+
+        }
+
+        public void ServiceSend(string message, string Id)
+        {
+            Clients.Client(Id).sendMessage(message);
+        }
+
+        public void ClientSend(string message, string Id)
+        {
+            Clients.Client(serviceID).sendMessage(message, Id);
+        }
+    }
+}
---
+++ prj_chuju/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(prj_chuju.Startup))]
+
+namespace prj_chuju
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
